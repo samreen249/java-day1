d2643fa0810458d0815a1bf4e1559d98c25cebcd
samreen249 <shaiksamreen1119@gmail.com>
2026-01-31 15:29:20 +0530

    Add files via upload

--- Input.java
+++ Input.java
@@ -3,6 +3,14 @@ public class Input {
     public static void main(String[] args) {
     Scanner sc=new Scanner(System.in);
     int age=sc.nextInt();
-    System.out.println(age);
+    double a=sc.nextDouble();
+    float b=sc.nextFloat();
+    String str1=sc.next();
+    String str2=sc.nextLine();
+System.out.println(age);
+System.out.println(b);
+System.out.println(a);
+System.out.println(str1);
+System.out.println(str2);
     }
 }
