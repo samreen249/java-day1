3a8a5b6e1192aafbf03dba4d920f0dc51011edbc
samreen249 <shaiksamreen1119@gmail.com>
2026-01-31 15:03:25 +0530

    Add files via upload

---
+++ Input.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Input {
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int age=sc.nextInt();
+    System.out.println(age);
+    }
+}
