4b002d655d9c69275ec4a4aa561541b256061e9b
samreen249 <shaiksamreen1119@gmail.com>
2026-01-31 14:04:28 +0530

    Add files via upload

---
+++ Demo1.java
@@ -0,0 +1,10 @@
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.print("samreen");
+        System.out.println("bindu");
+        System.out.print("bhavya");
+        System.out.println("rekha");
+        System.out.println("muskan");
+    }
+    
+}
